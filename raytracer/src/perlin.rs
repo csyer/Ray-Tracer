916eba8f663b59ad8b44c7eb8b0e757f035c900e
csyer <2960069894@qq.com>
2023-07-10 10:54:04 +0800

    book2 image8: Perlin texture with trilinear interpolation

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -27,15 +27,43 @@ impl Perlin {
         p
     }
 
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for (i, ci) in c.iter().enumerate() {
+            for (j, cj) in ci.iter().enumerate() {
+                for (k, ck) in cj.iter().enumerate() {
+                    accum += ((i as f64) * u + (1.0 - (i as f64)) * (1.0 - u))
+                        * ((j as f64) * v + (1.0 - (j as f64)) * (1.0 - v))
+                        * ((k as f64) * w + (1.0 - (k as f64)) * (1.0 - w))
+                        * *ck;
+                }
+            }
+        }
+
+        accum
+    }
+
     pub fn noise(&self, p: Point3) -> f64 {
-        let i = ((4.0 * p.x()) as i32) & 255;
-        let j = ((4.0 * p.y()) as i32) & 255;
-        let k = ((4.0 * p.z()) as i32) & 255;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        // println!("({}, {}, {}", p.x(), p.y(), p.z());
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
 
-        self.ranfloat
-            [(self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]) as usize]
+        for (di, ci) in c.iter_mut().enumerate() {
+            for (dj, cj) in ci.iter_mut().enumerate() {
+                for (dk, ck) in cj.iter_mut().enumerate() {
+                    *ck = self.ranfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]];
+                }
+            }
+        }
+
+        Perlin::trilinear_interp(c, u, v, w)
     }
 }
 
