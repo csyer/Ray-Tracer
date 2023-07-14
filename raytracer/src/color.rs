b850847b38ed5468554bb50b4690b9a91c18e712
csyer <2960069894@qq.com>
2023-07-14 11:10:08 +0800

    book3 image12: Cornell box with anti-acne color function

--- raytracer/src/color.rs
+++ raytracer/src/color.rs
@@ -21,6 +21,16 @@ pub fn write_color(img: &mut RgbImage, pos: Position, rgb: Color, samples_per_pi
     let mut g: f64 = rgb.y();
     let mut b: f64 = rgb.z();
 
+    if r.is_nan() {
+        r = 0.0;
+    }
+    if g.is_nan() {
+        g = 0.0;
+    }
+    if b.is_nan() {
+        b = 0.0;
+    }
+
     // Divide the color by the number of samples and gamma-correct for gamma=2.0.
     let scale = 1.0 / (samples_per_pixel as f64);
     r = (r * scale).sqrt();
--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -143,7 +143,7 @@ fn cornell_box() -> HittableList {
 }
 
 fn main() {
-    let path = std::path::Path::new("output/book3/image11.jpg");
+    let path = std::path::Path::new("output/book3/image12.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
